695e9f928edfa16b5ddfa59c490b5a2f933d27b7
Maggie <maggiesun625@gmail.com>
2025-11-26 19:46:02 -0900

    added 3rd conv layer, did not fix

--- cnn_model/train.py
+++ cnn_model/train.py
@@ -53,22 +53,40 @@ class EEGDataset(Dataset):
 class BaseEEGCNN(nn.Module):
     def __init__(self, num_classes=2):
         super().__init__()
-        self.conv1 = nn.Conv1d(3, 16, kernel_size=64, stride=16)
+        #block1
+        self.conv1 = nn.Conv1d(3, 32, kernel_size=64, stride=16, padding = 0)
+        self.bn1 = nn.BatchNorm1d(32)
         self.pool1 = nn.MaxPool1d(4)
 
-        self.conv2 = nn.Conv1d(16, 32, 3)
-        self.pool2 = nn.MaxPool1d(4)
+        self.conv2 = nn.Conv1d(32, 64, kernel_size = 8, stride = 2, padding =3)
+        self.bn2 = nn.BatchNorm1d(64)
+        self.pool2 = nn.MaxPool1d(2)
 
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(32 * 22, 64)
+        self.conv3 = nn.Conv1d(64, 128, kernel_size = 8, stride = 2, padding =3)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.pool3 = nn.MaxPool1d(2)
+
+        self.global_avg = nn.AdaptiveAvgPool1d(1)
+
+        self.dropout = nn.Dropout(0.5)
+        
+        self.fc1 = nn.Linear(128, 64)
         self.fc2 = nn.Linear(64, num_classes)
 
+        self.relu = nn.ReLU()
+
+
     def forward(self, x):
-        x = self.pool1(self.relu(self.conv1(x)))
-        x = self.pool2(self.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)
-        x = self.relu(self.fc1(x))
-        return self.fc2(x)
+        x = self.pool1(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool2(self.relu(self.bn2(self.conv2(x))))
+        x = self.pool3(self.relu(self.bn3(self.conv3(x))))
+
+        x = self.global_avg(x)
+        x = x.squeeze(-1)
+
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return x
 
 
 # 5. Minimal training loop
@@ -83,7 +101,7 @@ def train_minimal(model, train_loader, test_loader, device, epochs=10, lr=1e-3, 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     #NEW CLEAN TENSORBOARD RUN
-    run_name = f"rolled_back_weights+threshold_{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
+    run_name = f"deeper_model_from_rollback_{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
     writer = SummaryWriter(log_dir=f"runs/{run_name}")
 
     for epoch in range(epochs):
